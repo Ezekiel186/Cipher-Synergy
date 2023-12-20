02985bd396bd4ccbf1c8f38667d6de10979d3153
Ezekiel <ezekieljamolin186@gmail.com>
2023-12-20 13:39:18 -0800

    Made functionality to buttons

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,12 @@ const creditsInfo = document.querySelector('.credit-info');
 const backBtnRules = document.getElementById('back1');
 const backBtnCredits = document.getElementById('back2');
 
+const player1 = document.getElementById('button1');
+const player2 = document.getElementById('button2');
+const home = document.querySelector('.home');
+const room1 = document.querySelector('.room-one');
+const room2 = document.querySelector('.room-two');
+
 ruleButton.addEventListener('click', function() {
     rulesInfo.classList.remove('hide');
     buttonContainer.classList.add('hide');
@@ -35,4 +41,16 @@ backBtnRules.addEventListener('click', function() {
 backBtnCredits.addEventListener('click', function() {
     creditsInfo.classList.add('hide');
     buttonContainer.classList.remove('hide');
+});
+
+// player 1 room
+
+player1.addEventListener('click', function() {
+    home.classList.add('hide');
+    room1.classList.remove('hide');
+});
+
+player2.addEventListener('click', function() {
+    home.classList.add('hide');
+    room2.classList.remove('hide');
 });
